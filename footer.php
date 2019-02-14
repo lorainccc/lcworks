a7fba4c4e1bb6618f5b0d7bbdf729e3787752bcb
Joe Querin <7998323+joecue@users.noreply.github.com>
2019-02-14 15:20:32 -0500

    Update to Directions and Map Link

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 		<p>Copyright &copy; <?php echo date("Y"); ?> | OhioMeansJobs Lorain County</p>
-		<p>42495 North Ridge Road, Elyria, Ohio 44035 | Phone: 440-324-5244 | <a href="mailto:jgonzales@loraincounty.us;%20mlongo@loraincounty.us">Contact Us</a> | <a title="Directions and Map" href="/Directions.htm">Directions and Map</a></p>	
+		<p>42495 North Ridge Road, Elyria, Ohio 44035 | Phone: 440-324-5244 | <a href="mailto:jgonzales@loraincounty.us;%20mlongo@loraincounty.us">Contact Us</a> | <a title="Directions and Map" href="/directions-map/">Directions and Map</a></p>	
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
